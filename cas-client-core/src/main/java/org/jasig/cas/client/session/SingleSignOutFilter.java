a646643e0524a9fcac1072114eb2548ed545ff82
Florian Ramillien <florian.ramillien@almerys.com>
2014-04-15 11:38:15 +0200

    Typo

--- cas-client-core/src/main/java/org/jasig/cas/client/session/SingleSignOutFilter.java
+++ cas-client-core/src/main/java/org/jasig/cas/client/session/SingleSignOutFilter.java
@@ -70,7 +70,7 @@ public final class SingleSignOutFilter extends AbstractConfigurationFilter {
             // Do not continue up filter chain
             return;
         } else {
-			handler.refreshSession(request);
+            handler.refreshSession(request);
             log.trace("Ignoring URI " + request.getRequestURI());
         }
 
